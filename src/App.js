dc8e2e46e009407fd962ef7cc0eae56b10fd8a77
jooon1008 <kimjooon1008@gmail.com>
2026-08-25 13:13:55 +0900

    feat: 입력에 대한 예외검증 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
+
+  //view
+  validateCarInput(splitted){
+    for (const name of splitted) {
+      if (!name.trim()) {
+        throw new Error('[ERROR] 자동차 이름은 비어 있을 수 없습니다.');
+      }
+      if (/\s/.test(name)){
+        throw new Error('[ERROR] 자동차 이름은 공백이 포함될 수 없습니다.');
+      }
+      if ( name.length > 5){
+        throw new Error('[ERROR] 자동차 이름은 5글자 이하여야 합니다.');
+      }
+    }
+  }
+
+  validateMoveInput(input){
+    if (!/^[1-9]\d*$/.test(input)){
+      throw new Error('[ERROR] 시도할 횟수는 양수로만 구성되어야 합니다.');
+    }
+  }
+
   async carInput(){
     const input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.( 이름은 쉼표 (,) 기준으로 구분)\n');
     const splitted = input.split(',');
     const cars = new Array();
+    //검증
+    this.validateCarInput(splitted);
 
-    for (const name of splitted){
+    for (const name of splitted) {
       cars.push({name: name , position: 0});
     }
 
     return cars;
   }
+
   async moveInput(){
     const input = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    const moveCount = parseInt(input);
+    //검증
+    this.validateMoveInput(input);
 
+    const moveCount = parseInt(input);
     return moveCount;
   }
 
+  //model
+  
+
+
+  //controller
   async run() {
     const cars = await this.carInput();
     const moveCount = await this.moveInput();
+
 
   }
 }
