72ad8f419eeb6a381ae0e3923daf8065f0c8e637
jooon1008 <kimjooon1008@gmail.com>
2026-08-25 12:43:36 +0900

    feat: 자동차 이름과 시도 횟수 입력 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async carInput(){
+    const input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.( 이름은 쉼표 (,) 기준으로 구분)\n');
+    const splitted = input.split(',');
+    const cars = new Array();
+
+    for (const name of splitted){
+      cars.push({name: name , position: 0});
+    }
+
+    return cars;
+  }
+  async moveInput(){
+    const input = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const moveCount = parseInt(input);
+
+    return moveCount;
+  }
+
+  async run() {
+    const cars = await this.carInput();
+    const moveCount = await this.moveInput();
+
+  }
 }
 
 export default App;
